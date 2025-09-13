0a50779b64e2d7ab80d2ab7cdd40a5a134e678f0
HidayatPatil <hpatil2@my.bcit.ca>
2025-09-13 13:08:08 -0700

    changed image locations to public/assets

--- .history/src/App_20250913130621.jsx
+++ .history/src/App_20250913130621.jsx
@@ -22,7 +22,7 @@ function App() {
       <div className="book_listing">
         <div className="add_new">New</div>
         <Book image="public/assets/book1.png" author="David Nixon" link="https://itbook.store/books/9781484256381" />
-        <Book image="public/assets/book2.png" author="Janine Suvak" link="https://itbook.store/books/9781430265863" />
+        <Book image="src/assets/book2.png" author="Janine Suvak" link="https://itbook.store/books/9781430265863" />
       </div>
       <div className="footer">Hidayat Patil, 2025</div>
     </div>
